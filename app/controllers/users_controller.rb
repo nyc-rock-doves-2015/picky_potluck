da3ea682a7b98707942db59511aff5c5ac53fc76
Debbie Milburn <deborah.a.milburn@gmail.com>
2015-05-07 17:12:23 -0400

    Add empty sessions_controller and users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+
   def index
     @users = User.all
   end
@@ -6,4 +7,5 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
 end
