3722fdbc051bab2ca549469eae3b51ba915e7c66
Debbie Milburn <deborah.a.milburn@gmail.com>
2015-05-07 17:29:00 -0400

    Add working signin/signout functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,21 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(name: params[:session][:name])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:notice] = "Logged in!"
+      redirect_to root_url
+    else
+      flash[:notice] = "Invalid username or password"
+      redirect_to root_url
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    flash[:notice] = "Logged out!"
+    redirect_to root_url
   end
 
 end
