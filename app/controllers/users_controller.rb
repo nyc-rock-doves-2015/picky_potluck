0746ef4e4cf610489f8a91a7afa5977cd5be1b35
Debbie Milburn <deborah.a.milburn@gmail.com>
2015-05-07 17:14:39 -0400

    Add empty new and create methods for users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,17 @@
 class UsersController < ApplicationController
 
+
   def index
     @users = User.all
   end
 
   def show
     @user = User.find(params[:id])
+
+  def new
+  end
+
+  def create
   end
 
 end
