20d98234422dbd017c2879e3a5147fc2c9ebb93c
Debbie Milburn <deborah.a.milburn@gmail.com>
2015-05-07 18:01:06 -0400

    Add users create method logic. Working signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,20 @@ class UsersController < ApplicationController
   end
 
   def create
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      flash[:notice] = "Thank you for signing up!"
+      redirect_to root_url
+    else
+      render "new"
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :email)
   end
 
 end
