febef25794a996d4d80b74e3ed45cb2bd7d81d17
Lucian Kahn <luciankahn@gmail.com>
2015-05-08 13:44:53 -0400

    add type field to create_nonos migration, to handle ingredients, allergies, or vegetarian when sending to search query

--- db/migrate/20150507202732_create_nonos.rb
+++ db/migrate/20150507202732_create_nonos.rb
@@ -1,7 +1,12 @@
 class CreateNonos < ActiveRecord::Migration
   def change
     create_table :nonos do |t|
-      t.string :name
+      t.string :name, null: false
+      t.string :type, null: false
+      # possible values for type:
+      # ingredient (q= excludedIngredient[])
+      # allergy (q= allowedAllergy[])
+      # vegetarian (q= allowedDiet[])
 
       t.timestamps
     end
