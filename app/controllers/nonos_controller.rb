1d4f9a0cbb3776986022172d980452fc974a653f
Harry Park <imharrypark@gmail.com>
2015-05-08 16:39:35 -0400

    create hashes for allergies and vegetarian options search codes in Nono model

--- app/controllers/nonos_controller.rb
+++ app/controllers/nonos_controller.rb
@@ -1,22 +1,3 @@
 class NonosController < ApplicationController
-# allergies
-# "396^Dairy-Free"
-# "397^Egg-Free"
-# "393^Gluten-Free"
-# "394^Peanut-Free"
-# "398^Seafood-Free"
-# "399^Sesame-Free"
-# "400^Soy-Free"
-# "401^Sulfite-Free"
-# "395^Tree Nut-Free"
-# "392^Wheat-Free"
-
-# diets
-# "388^Lacto vegetarian"
-# "389^Ovo vegetarian"
-# "390^Pescetarian"
-# "386^Vegan"
-# "387^Lacto-ovo vegetarian"
-# "403^Paleo"
 
 end
--- app/models/nono.rb
+++ app/models/nono.rb
@@ -6,4 +6,27 @@ class Nono < ActiveRecord::Base
     # ingredient (q= excludedIngredient[])
     # allergy (q= allowedAllergy[])
     # vegetarian (q= allowedDiet[])
+
+  ALLERGIES = {
+    dairy: "396^Dairy-Free",
+    egg: "397^Egg-Free",
+    gluten: "393^Gluten-Free",
+    peanut: "394^Peanut-Free",
+    seafood: "398^Seafood-Free",
+    sesame: "399^Sesame-Free",
+    soy: "400^Soy-Free",
+    sulfite: "401^Sulfite-Free",
+    tree_nut: "395^Tree Nut-Free",
+    wheat: "392^Wheat-Free"
+  }
+
+  VEG_OPTIONS = {
+    lacto_veg: "388^Lacto vegetarian",
+    ovo_veg: "389^Ovo vegetarian",
+    pescetarian: "390^Pescetarian",
+    vegan: "386^Vegan",
+    vegetarian: "387^Lacto-ovo vegetarian",
+    paleo: "403^Paleo"
+  }
+
 end
