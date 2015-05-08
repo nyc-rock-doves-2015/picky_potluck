939ed76ff42fda11fc5a815b91d222ff19ed1781
Lucian Kahn <luciankahn@gmail.com>
2015-05-08 13:40:47 -0400

    create combine_nonos method in Party model

--- app/models/party.rb
+++ app/models/party.rb
@@ -1,4 +1,16 @@
 class Party < ActiveRecord::Base
   has_many :rsvps
   has_many :users, through: :rsvps
+
+  def combine_nonos
+    @users = self.users
+    @combined_nonos = []
+    @users.each do |user|
+      user.nonos.each do |nono|
+        @combined_nonos << nono
+      end
+    end
+    @combined_nonos.uniq
+  end
+
 end
