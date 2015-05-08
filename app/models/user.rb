c235b74f75a25c81ca575f403e5e75551a6d2bdd
Debbie Milburn <deborah.a.milburn@gmail.com>
2015-05-08 10:22:03 -0400

    Make user model validation for email presence

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
+  validates :email, presence: true
   validates :password, confirmation: true
   validates :password_confirmation, presence: true
 end
