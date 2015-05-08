b7632fcbc14182ceed274d5b0615b5375a37db45
Debbie Milburn <deborah.a.milburn@gmail.com>
2015-05-07 20:17:25 -0400

    Change user signin to email vs. name and bad signin redirects to signin path

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,14 +7,14 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(name: params[:session][:name])
+    user = User.find_by(email: params[:session][:email])
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
       flash[:notice] = "Logged in!"
       redirect_to root_url
     else
-      flash[:notice] = "Invalid username or password"
-      redirect_to root_url
+      flash[:notice] = "Invalid email or password"
+      redirect_to signin_path
     end
   end
 
