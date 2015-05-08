a1756a20d47968ba96650051d0f8d6bf850c704a
Harry Park <imharrypark@gmail.com>
2015-05-08 15:57:32 -0400

    make note of allergy keys

---
+++ app/controllers/nonos_controller.rb
@@ -0,0 +1,14 @@
+class NonosController < ApplicationController
+
+# "396^Dairy-Free"
+# "397^Egg-Free"
+# "393^Gluten-Free"
+# "394^Peanut-Free"
+# "398^Seafood-Free"
+# "399^Sesame-Free"
+# "400^Soy-Free"
+# "401^Sulfite-Free"
+# "395^Tree Nut-Free"
+# "392^Wheat-Free"
+
+end
