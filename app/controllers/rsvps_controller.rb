1a15a6f9ef3e804b43bc4e88db415ad2a69d01ba
Debbie Milburn <deborah.a.milburn@gmail.com>
2015-05-08 16:08:09 -0400

    Create create_notice method

--- app/controllers/rsvps_controller.rb
+++ app/controllers/rsvps_controller.rb
@@ -1,7 +1,6 @@
 class RsvpsController < ApplicationController
 
   def create
-    notice = ""
     good_emails = []
     bad_emails = []
     party = Party.find(params[:party_id])
@@ -15,10 +14,16 @@ class RsvpsController < ApplicationController
         bad_emails << email_address
       end
     end
-    notice += "Invitations sent to #{good_emails.join(' ')}. " if good_emails.length > 0
-    notice += "Could not locate Picky Potluck members for #{bad_emails.join(', ')}. " if bad_emails.length > 0
-    flash[:notice] = notice
+    flash[:notice] = create_notice(good_emails, bad_emails)
     redirect_to party_path(party)
+  end
+
+  private
+
+  def create_notice(good_emails, bad_emails)
+    notice = ""
+    notice += "Invitations sent to #{good_emails.join(' ')}. " if good_emails.length > 0
+    notice += "Could not locate Picky Potluck memberships for #{bad_emails.join(', ')}. " if bad_emails.length > 0
   end
 
 end
