e57f7df4db072b7e026648262996885685390b74
Harry Park <imharrypark@gmail.com>
2015-05-08 16:07:50 -0400

    make note of allowedDiet keys

--- app/controllers/nonos_controller.rb
+++ app/controllers/nonos_controller.rb
@@ -1,5 +1,5 @@
 class NonosController < ApplicationController
-
+# allergies
 # "396^Dairy-Free"
 # "397^Egg-Free"
 # "393^Gluten-Free"
@@ -10,5 +10,13 @@ class NonosController < ApplicationController
 # "401^Sulfite-Free"
 # "395^Tree Nut-Free"
 # "392^Wheat-Free"
+
+# diets
+# "388^Lacto vegetarian"
+# "389^Ovo vegetarian"
+# "390^Pescetarian"
+# "386^Vegan"
+# "387^Lacto-ovo vegetarian"
+# "403^Paleo"
 
 end
