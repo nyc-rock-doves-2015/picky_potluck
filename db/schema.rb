65854b9c15755d1766d48f3ba059e071e46dce09
Lucian Kahn <luciankahn@gmail.com>
2015-05-09 10:18:59 -0400

    add new migrations to schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150509140223) do
+ActiveRecord::Schema.define(version: 20150509141244) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "chosen_recipes", force: :cascade do |t|
+    t.integer  "recipe_id",  null: false
+    t.integer  "rsvp_id",    null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "nono_users", force: :cascade do |t|
     t.integer  "user_id",    null: false
