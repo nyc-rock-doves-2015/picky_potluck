3cc731623ed66f4a837aacc03f1dd03a6d084f11
Lucian Kahn <luciankahn@gmail.com>
2015-05-09 17:05:30 -0400

    add hidden form to get image url of result dish that is chosen in order to create claimed_dish

--- app/controllers/claimed_dishes_controller.rb
+++ app/controllers/claimed_dishes_controller.rb
@@ -1,6 +1,10 @@
 class ClaimedDishesController < ApplicationController
   def create
     party = Party.find(params[:party_id])
+    rsvp = party.rsvps.find_by(user_id: current_user.id)
+    p "HERE ARE THE PARAMS!!!!!!!!!"
+    p params
+    # rsvp.claimed_dish.create()
     redirect_to party_path(party)
   end
 end
