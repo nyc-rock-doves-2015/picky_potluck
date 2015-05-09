e7c60a49986715cca3d1698f18c82067c5c61608
Debbie Milburn <deborah.a.milburn@gmail.com>
2015-05-09 14:31:38 -0400

    Refactor user model test to check for attributes

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,10 @@
 require 'rails_helper'
 describe User do
   let(:user) { FactoryGirl.build :user }
-  it "should have an email" do
+
+  it "should have an email, name and password" do
     expect(user.email).to eq "test@test.com"
+    expect(user.name).to eq "testie"
+    expect(user.password).to eq "password"
   end
 end
