134c75f9637cca67be07ff72e3c3cc43cd9e485c
Debbie Milburn <deborah.a.milburn@gmail.com>
2015-05-09 15:03:31 -0400

    Add party model test to check if party saves with good attributes

--- spec/models/party_spec.rb
+++ spec/models/party_spec.rb
@@ -10,5 +10,9 @@ describe Party do
       expect(party.location).to eq "New York"
     end
 
+    it "should save to the database with good attributes" do
+      party.save
+      expect(Party.count).to eq(1)
+    end
   end
 end
