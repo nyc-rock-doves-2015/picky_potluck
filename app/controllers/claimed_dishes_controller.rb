ae77a29646da3e931b4c6a4264c0ca2d60ae2655
Lucian Kahn <luciankahn@gmail.com>
2015-05-09 17:15:58 -0400

    successfully creating claimed dish record in db from query_results form

--- app/controllers/claimed_dishes_controller.rb
+++ app/controllers/claimed_dishes_controller.rb
@@ -2,9 +2,9 @@ class ClaimedDishesController < ApplicationController
   def create
     party = Party.find(params[:party_id])
     rsvp = party.rsvps.find_by(user_id: current_user.id)
-    p "HERE ARE THE PARAMS!!!!!!!!!"
-    p params
-    # rsvp.claimed_dish.create()
+    photo_url = params[:photo_url]
+    photo_url = nil if photo_url == ""
+    ClaimedDish.create(rsvp_id: rsvp.id, yummly_id: params[:claimed_dish][:yummly_id], photo_url: photo_url)
     redirect_to party_path(party)
   end
 end
