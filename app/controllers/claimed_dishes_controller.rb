71c461e1e05568be19f611fc7a0449046ce47de6
Lucian Kahn <luciankahn@gmail.com>
2015-05-09 17:47:18 -0400

    Include debugging tools for image_url bug

--- app/controllers/claimed_dishes_controller.rb
+++ app/controllers/claimed_dishes_controller.rb
@@ -3,6 +3,8 @@ class ClaimedDishesController < ApplicationController
     party = Party.find(params[:party_id])
     rsvp = party.rsvps.find_by(user_id: current_user.id)
     photo_url = params[:photo_url]
+    p "AAAAAAAAA!!!!!!!"
+    p photo_url
     photo_url = nil if photo_url == ""
     ClaimedDish.create(rsvp_id: rsvp.id, yummly_id: params[:claimed_dish][:yummly_id], photo_url: photo_url)
     redirect_to party_path(party)
