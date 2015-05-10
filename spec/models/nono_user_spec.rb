0a935201597d7c79a0acc70cb49d1bce592c227d
Lucian Kahn <luciankahn@gmail.com>
2015-05-10 12:31:29 -0400

    nono_user is invalid without user_id or nono_id

--- spec/models/nono_user_spec.rb
+++ spec/models/nono_user_spec.rb
@@ -5,7 +5,14 @@ describe NonoUser do
     nono_user = NonoUser.new(user_id: Faker::Address.building_number, nono_id: Faker::Address.building_number)
     expect(nono_user).to be_valid
   end
-  it "is invalid without a user_id"
-  it "is invalid without a nono-id"
-
+  it "is invalid without a user_id" do
+    nono_user = NonoUser.new(user_id: nil)
+    nono_user.valid?
+    expect(nono_user.errors[:user_id]).to include("can't be blank")
+  end
+  it "is invalid without a nono-id" do
+    nono_user = NonoUser.new(nono_id: nil)
+    nono_user.valid?
+    expect(nono_user.errors[:nono_id]).to include("can't be blank")
+  end
 end
