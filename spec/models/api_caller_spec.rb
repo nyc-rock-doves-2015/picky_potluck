b24e18e173e98d057a1c606804d291436b3b2337
Lucian Kahn <luciankahn@gmail.com>
2015-05-10 14:28:58 -0400

    write list of tests for api_caller_spec

---
+++ spec/models/api_caller_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+describe ApiCaller do
+  it "changes spaces to + in the input search term and includes that version in the query"
+  it "adds parameters for ingredients after the ingredients query method"
+  it "adds parameters for vegetarian options after the vegetarian options query method"
+  it "adds parameters for allergies after the allergies query method"
+end
