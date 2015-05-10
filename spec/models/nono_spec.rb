b3d9a5e1b792354de0687b0335344e30d1e6e629
Lucian Kahn <luciankahn@gmail.com>
2015-05-10 12:18:24 -0400

    is invalid without a yummly_code if the category is 'vegetarian'

--- spec/models/nono_spec.rb
+++ spec/models/nono_spec.rb
@@ -32,6 +32,13 @@ describe Nono do
     expect(nono.errors[:category]).to include("can't be blank")
   end
 
+  it "is valid without a yummly_code if the category is 'ingredient'" do
+    nono = Nono.new(
+      name: Faker::Name.name,
+      category: 'ingredient')
+    expect(nono).to be_valid
+  end
+
   it "is invalid if the category is something other than 'allergy', 'vegetarian', or 'ingredient'" do
     nono = Nono.new(category: Faker::Company.name)
     nono.valid?
@@ -44,7 +51,12 @@ describe Nono do
     expect(nono.errors[:yummly_code]).to include("can't be blank")
   end
 
-  it "is invalid without a yummly_code if the category is 'vegetarian'"
+  it "is invalid without a yummly_code if the category is 'vegetarian'" do
+    nono = Nono.new(category: "vegetarian", yummly_code: nil)
+    nono.valid?
+    expect(nono.errors[:yummly_code]).to include("can't be blank")
+  end
+
   it "returns a yummly_code as a string"
 
 end
