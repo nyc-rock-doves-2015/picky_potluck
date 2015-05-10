b3035205b3f5d42409c6741277517c79770be792
Debbie Milburn <deborah.a.milburn@gmail.com>
2015-05-10 13:07:43 -0400

    Create current_user_guest_list_check method in party_controller

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -1,15 +1,11 @@
 class PartiesController < ApplicationController
   def show
     @party = Party.find(params[:id])
-    if current_user.on_guest_list?(@party)
-      @rsvp = Rsvp.new
-      @location_query = @party.location.split(' ').join('+')
-      @attendees = @party.users
-      @restrictions = @party.combine_nonos
-    else
-      flash[:notice] = "Sorry, you don't have permission to see that."
-      redirect_to user_path(current_user)
-    end
+    current_user_guest_list_check(@party)
+    @rsvp = Rsvp.new
+    @location_query = @party.location.split(' ').join('+')
+    @attendees = @party.users
+    @restrictions = @party.combine_nonos     
   end
 
   def create
@@ -39,6 +35,13 @@ class PartiesController < ApplicationController
 
   def party_params
     params.require(:party).permit(:name, :location, :date)
+  end
+
+  def current_user_guest_list_check(party)
+    if !current_user.on_guest_list?(party)
+     flash[:notice] = "Sorry, you don't have permission to see that."
+      redirect_to user_path(current_user)
+    end
   end
 
 end
