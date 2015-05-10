916522a9cd2080128e804aa89a4a8a7d257ca1ce
Debbie Milburn <deborah.a.milburn@gmail.com>
2015-05-10 16:47:46 -0400

    another experiment

--- spec/models/api_caller_spec.rb
+++ spec/models/api_caller_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 describe ApiCaller do
-  let(:allergy_nono) { Nono.create(
+  let(:allergy_nono) { Nono.new(
       name: "whatever",
       category: 'allergy',
       yummly_code: "beets") }
-  let(:veg_nono) { Nono.create(
+  let(:veg_nono) { Nono.new(
       name: "whatever",
       category: 'vegetarian',
       yummly_code: "veg") }
@@ -16,9 +16,9 @@ describe ApiCaller do
     api_caller.generate_query([allergy_nono, veg_nono], "onion soup")
   }
 
-  # it "changes spaces to + in the input search term and includes that version in the query" do
-  #   expect(query).to start_with("&q=onion+soup")
-  # end
+  it "changes spaces to + in the input search term and includes that version in the query" do
+    expect(query).to start_with("&q=onion+soup")
+  end
 
   # it "adds parameters for allergies after the allergies query method" do
   #   split_query = query.split("soup")
