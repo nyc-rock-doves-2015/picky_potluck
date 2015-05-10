b25d9aaff143dace9e552b5756b5528a50504b66
Lucian Kahn <luciankahn@gmail.com>
2015-05-10 12:27:46 -0400

    NonoUser is valid with a user_id and nono_id

--- spec/models/nono_user_spec.rb
+++ spec/models/nono_user_spec.rb
@@ -1,7 +1,10 @@
-require 'rails-helper'
+require 'rails_helper'
 
 describe NonoUser do
-  it "is valid with a user_id and nono_id"
+  it "is valid with a user_id and nono_id" do
+    nono_user = NonoUser.new(user_id: Faker::Address.building_number, nono_id: Faker::Address.building_number)
+    expect(nono_user).to be_valid
+  end
   it "is invalid without a user_id"
   it "is invalid without a nono-id"
 
