2a9c9ec3c49bfc86c5f6bb91ddd0f6d54dae0412
Debbie Milburn <deborah.a.milburn@gmail.com>
2015-05-10 16:09:37 -0400

    Comment out all tests

--- spec/models/api_caller_spec.rb
+++ spec/models/api_caller_spec.rb
@@ -1,28 +1,28 @@
 require 'rails_helper'
 
 describe ApiCaller do
-  let(:allergy_nono) { Nono.create(
-      name: "whatever",
-      category: 'allergy',
-      yummly_code: "beets") }
-  let(:veg_nono) { Nono.create(
-      name: "whatever",
-      category: 'vegetarian',
-      yummly_code: "veg") }
-  let(:ingr_nono) { Nono.create(
-      name: "nuts",
-      category: 'ingredient',
-      yummly_code: Faker::Address.longitude) }
-  let(:api_caller) {
-    ApiCaller.new
-  }
-  let(:query) {
-    api_caller.generate_query([allergy_nono, veg_nono, ingr_nono], "onion soup")
-  }
+  # let(:allergy_nono) { Nono.create(
+  #     name: "whatever",
+  #     category: 'allergy',
+  #     yummly_code: "beets") }
+  # let(:veg_nono) { Nono.create(
+  #     name: "whatever",
+  #     category: 'vegetarian',
+  #     yummly_code: "veg") }
+  # let(:ingr_nono) { Nono.create(
+  #     name: "nuts",
+  #     category: 'ingredient',
+  #     yummly_code: Faker::Address.longitude) }
+  # let(:api_caller) {
+  #   ApiCaller.new
+  # }
+  # let(:query) {
+  #   api_caller.generate_query([allergy_nono, veg_nono, ingr_nono], "onion soup")
+  # }
 
-  it "changes spaces to + in the input search term and includes that version in the query" do
-    expect(query).to start_with("&q=onion+soup")
-  end
+  # it "changes spaces to + in the input search term and includes that version in the query" do
+  #   expect(query).to start_with("&q=onion+soup")
+  # end
 
   # it "adds parameters for ingredients after the ingredients query method" do
   #   split_query = query.split("soup")
