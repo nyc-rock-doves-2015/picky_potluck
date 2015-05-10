c3a49635bc5d4ff2eb0b4427def3a90790223bfc
Debbie Milburn <deborah.a.milburn@gmail.com>
2015-05-10 14:42:47 -0400

    Create claimed_dish method for Rsvp model

--- app/models/rsvp.rb
+++ app/models/rsvp.rb
@@ -8,4 +8,9 @@ class Rsvp < ActiveRecord::Base
 
   validates :user_id, uniqueness: {scope: :party_id, message: "rsvp must be unique"}
   validates :party_id, uniqueness: {scope: :user_id, message: "rsvp must be unique"}
+
+
+  def claimed_dish
+    ClaimedDish.find_by(rsvp_id: self.id)
+  end
 end
