3f21e6599e3113206fcd76ee2881bff9cb75348f
Lucian Kahn <luciankahn@gmail.com>
2015-05-10 12:42:00 -0400

    rsvp is invalid without a user_id or party_id

--- spec/models/rsvp_spec.rb
+++ spec/models/rsvp_spec.rb
@@ -5,7 +5,15 @@ describe Rsvp do
     rsvp = Rsvp.new(user_id: Faker::Address.building_number, party_id: Faker::Address.building_number)
     expect(rsvp).to be_valid
   end
-  it "is invalid without a user_id"
-  it "is invalid without a party_id"
+  it "is invalid without a user_id" do
+    rsvp = Rsvp.new(user_id: nil)
+    rsvp.valid?
+    expect(rsvp.errors[:user_id]).to include("can't be blank")
+  end
+  it "is invalid without a party_id" do
+    rsvp = Rsvp.new(party_id: nil)
+    rsvp.valid?
+    expect(rsvp.errors[:party_id]).to include("can't be blank")
+  end
   it "is invalid if BOTH the user_id and party_id belong to an existing Rsvp"
 end
