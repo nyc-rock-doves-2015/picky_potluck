f4b04028e34423350e0559f2eb4c4b8ab251fe98
Lucian Kahn <luciankahn@gmail.com>
2015-05-10 12:34:41 -0400

    add validates for presence of party_id and user_id in Rsvp model

--- app/models/rsvp.rb
+++ app/models/rsvp.rb
@@ -3,6 +3,9 @@ class Rsvp < ActiveRecord::Base
   belongs_to :user
   has_one :claimed_dish
 
+  validates :party_id, presence: true
+  validates :user_id, presence: true
+
   validates_uniqueness_of :user_id, :scope => [:party_id]
   validates_uniqueness_of :party_id, :scope => [:user_id]
 end
