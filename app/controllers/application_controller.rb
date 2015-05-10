e43549b2446b2c4635ef4b1c9aa31ec8a2b97de0
Debbie Milburn <deborah.a.milburn@gmail.com>
2015-05-10 12:20:29 -0400

    Add gate_keeper method as a before_action on everything except welcome page and enter page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,20 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :gate_keeper
 
   helper_method :current_user
 
   def current_user
     User.find_by(id: session[:user_id])
   end
+
+  def gate_keeper
+    if !current_user
+      flash[:notice] = "Please sign in to view that."
+      redirect_to enter_path
+    end
+  end
+
+
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
 
+  skip_before_action :gate_keeper
+
   def auth
     if current_user
       redirect_to user_path(current_user)
