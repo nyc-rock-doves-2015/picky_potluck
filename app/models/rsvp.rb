e6eeb2d203e26f4606aaf4a947f1686d2781d5a6
Debbie Milburn <deborah.a.milburn@gmail.com>
2015-05-10 14:54:20 -0400

    Change rsvp claimed_dish relationship to one to many

--- app/models/rsvp.rb
+++ app/models/rsvp.rb
@@ -1,7 +1,7 @@
 class Rsvp < ActiveRecord::Base
   belongs_to :party
   belongs_to :user
-  has_one :claimed_dish
+  has_many :claimed_dishes
 
   validates :party_id, presence: true
   validates :user_id, presence: true
@@ -9,8 +9,4 @@ class Rsvp < ActiveRecord::Base
   validates :user_id, uniqueness: {scope: :party_id, message: "rsvp must be unique"}
   validates :party_id, uniqueness: {scope: :user_id, message: "rsvp must be unique"}
 
-
-  def claimed_dish
-    ClaimedDish.find_by(rsvp_id: self.id)
-  end
 end
