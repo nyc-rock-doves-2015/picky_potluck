b00c7d2c7ad719634a232c25cbf771b92e26b1bb
Lucian Kahn <luciankahn@gmail.com>
2015-05-10 14:35:10 -0400

    add let to api_caller spec

--- spec/models/api_caller_spec.rb
+++ spec/models/api_caller_spec.rb
@@ -1,7 +1,23 @@
 require 'rails_helper'
 
 describe ApiCaller do
-  it "changes spaces to + in the input search term and includes that version in the query"
+  let(:allergy_nono) { Nono.new(
+      name: Faker::Name.name,
+      category: 'allergy',
+      yummly_code: Faker::Address.longitude) }
+  let(:veg_nono) { Nono.new(
+      name: Faker::Name.name,
+      category: 'vegetarian',
+      yummly_code: Faker::Address.longitude) }
+  let(:ingr_nono) { Nono.new(
+      name: Faker::Name.name,
+      category: 'ingredient',
+      yummly_code: Faker::Address.longitude) }
+
+  it "changes spaces to + in the input search term and includes that version in the query" do
+    api_caller = ApiCaller.new
+    # query = api_caller.generate_query()
+  end
   it "adds parameters for ingredients after the ingredients query method"
   it "adds parameters for vegetarian options after the vegetarian options query method"
   it "adds parameters for allergies after the allergies query method"
