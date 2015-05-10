92323df7edc850223a4bd95d924c4bfe14a5e7ea
Debbie Milburn <deborah.a.milburn@gmail.com>
2015-05-10 11:29:50 -0400

    Add view, route and controller for combined sign in and up page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,3 +1,6 @@
 class WelcomeController < ApplicationController
 
+  def auth
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,8 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
 
+  get 'enter' => 'welcome#auth', as: 'enter'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
