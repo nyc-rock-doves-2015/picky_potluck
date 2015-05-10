c28b5c1aa43f3a3f01012ddf35243c54198b87c0
Debbie Milburn <deborah.a.milburn@gmail.com>
2015-05-10 18:15:41 -0400

    Edit signin feature test wording to be more descriptive

--- spec/features/user_signin_spec.rb
+++ spec/features/user_signin_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
-feature "User can signin" do 
+feature "User trying to sign into the website" do 
   let!(:user) { FactoryGirl.create :user }
-  it "should be able to fill out a signin form and signin to the website" do
+  it "should be able to fill out a signin form with correct email and password and be redirected to their homepage" do
     visit enter_path
     fill_in 'session_email',   with: user.email
     fill_in 'session_password', with: user.password
