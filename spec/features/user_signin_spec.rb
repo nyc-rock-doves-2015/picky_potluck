92784fa5d7bbc404d6d4abc23082b0a5c4c12c1f
Debbie Milburn <deborah.a.milburn@gmail.com>
2015-05-10 18:18:42 -0400

    Create signin feature test for bad signin credentials

--- spec/features/user_signin_spec.rb
+++ spec/features/user_signin_spec.rb
@@ -10,4 +10,14 @@ feature "User trying to sign into the website" do
     expect(page).to have_content user.name
   end
 
+  it "should be able to fill out a signin form with incorrect email and password and be redirected back to sign in page" do
+    visit enter_path
+    fill_in 'session_email',   with: "potato"
+    fill_in 'session_password', with: "fussybuckets"
+    click_button "Sign In"
+    expect(page).to have_content "Sign In"
+  end
+
+
+
 end
