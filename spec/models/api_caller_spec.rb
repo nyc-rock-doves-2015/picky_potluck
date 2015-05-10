83d7bb506714a0919a4ace97b1d506856e2199aa
Lucian Kahn <luciankahn@gmail.com>
2015-05-10 14:49:56 -0400

    api_caller changes spaces to + in the input search term and includes that version in the query

--- spec/models/api_caller_spec.rb
+++ spec/models/api_caller_spec.rb
@@ -1,23 +1,25 @@
 require 'rails_helper'
 
 describe ApiCaller do
-  let(:allergy_nono) { Nono.new(
+  let(:allergy_nono) { Nono.create(
       name: Faker::Name.name,
       category: 'allergy',
       yummly_code: Faker::Address.longitude) }
-  let(:veg_nono) { Nono.new(
+  let(:veg_nono) { Nono.create(
       name: Faker::Name.name,
       category: 'vegetarian',
       yummly_code: Faker::Address.longitude) }
-  let(:ingr_nono) { Nono.new(
+  let(:ingr_nono) { Nono.create(
       name: Faker::Name.name,
       category: 'ingredient',
       yummly_code: Faker::Address.longitude) }
 
   it "changes spaces to + in the input search term and includes that version in the query" do
     api_caller = ApiCaller.new
-    # query = api_caller.generate_query()
+    query = api_caller.generate_query([allergy_nono, veg_nono, ingr_nono], "onion soup")
+    expect(query).to start_with("&q=onion+soup")
   end
+
   it "adds parameters for ingredients after the ingredients query method"
   it "adds parameters for vegetarian options after the vegetarian options query method"
   it "adds parameters for allergies after the allergies query method"
