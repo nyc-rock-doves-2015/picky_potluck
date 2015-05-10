9cb0eeb0f2b1047f2ab2bc45babcf6b1bb1f5879
Lucian Kahn <luciankahn@gmail.com>
2015-05-10 12:40:14 -0400

    is valid with a user_id and party_id

--- spec/models/rsvp_spec.rb
+++ spec/models/rsvp_spec.rb
@@ -1,7 +1,10 @@
 require 'rails_helper'
 
 describe Rsvp do
-  it "is valid with a user_id and party_id"
+  it "is valid with a user_id and party_id" do
+    rsvp = Rsvp.new(user_id: Faker::Address.building_number, party_id: Faker::Address.building_number)
+    expect(rsvp).to be_valid
+  end
   it "is invalid without a user_id"
   it "is invalid without a party_id"
   it "is invalid if BOTH the user_id and party_id belong to an existing Rsvp"
