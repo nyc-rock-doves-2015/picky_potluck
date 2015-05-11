eb8462c4e963fc906cdc862387d5ff37ddd232b9
Debbie Milburn <deborah.a.milburn@gmail.com>
2015-05-11 14:45:40 -0400

    Create selected html class to toggle between background color green and red on recipe divs

--- app/assets/javascripts/query_results.js
+++ app/assets/javascripts/query_results.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
   $('.result-recipe').click(function(event) {
     $event = $(event.target);
-    $event.closest('.result-recipe').css("background", "green");
+    $event.closest('.result-recipe').toggleClass("selected");
     // change the background color of this recipe's .result-recipe div.
     // when submit button is clicked, it creates a claimeddish with the correct information.
   })
