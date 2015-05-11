024760f41cb036c267f81afc5708daa6ed05c914
Debbie Milburn <deborah.a.milburn@gmail.com>
2015-05-11 15:11:44 -0400

    Write notes in js file

--- app/assets/javascripts/query_results.js
+++ app/assets/javascripts/query_results.js
@@ -2,8 +2,14 @@ $(document).ready(function () {
   $('.result-recipe').click(function(event) {
     $event = $(event.target);
     $event.closest('.result-recipe').toggleClass("selected");
+
     // change the background color of this recipe's .result-recipe div.
     // when submit button is clicked, it creates a claimeddish with the correct information.
   })
+
+  //Unselect other divs when one is selected
+  //Populate form with information from selected div
+
+
   $(".owl-carousel").owlCarousel();
 })
