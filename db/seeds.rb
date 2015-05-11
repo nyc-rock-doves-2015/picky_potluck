3b62ceb451969e71dce719e0e2fafb5d62f65d06
Debbie Milburn <deborah.a.milburn@gmail.com>
2015-05-11 09:59:06 -0400

    Change seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-5.times do
-  party = Party.create(name: Faker::Company.name, date: Faker::Date.forward(20), location: 'Downtown, Manhattan')
-  5.times do
-    user = party.users.create(email: Faker::Internet.email, password: 'password', password_confirmation: 'password', name: Faker::Name.name, photo_url: Faker::Avatar.image, fave: 'lemon, pepper, onion, tomatoes')
+Party.create(name: "Beccy's Birthday", date: Faker::Date.forward(20), location: '48 Wall Street, New York, NY')
+
+Party.create(name: "Our Anniversary", date: Faker::Date.forward(20), location: '48 Wall Street, New York, NY')
+
+Party.create(name: "Graduation Party!", date: Faker::Date.forward(20), location: '48 Wall Street, New York, NY')
+
+
+Party.create(name: "Summer BBQ", date: Faker::Date.forward(20), location: '48 Wall Street, New York, NY')
+
+Party.create(name: "Graduation Bash", date: Faker::Date.forward(20), location: '48 Wall Street, New York, NY')
+
+Party.create(name: "Juan's going away party", date: Faker::Date.forward(20), location: '48 Wall Street, New York, NY')
+
+Party.create(name: "Welcome Home, Denise!", date: Faker::Date.forward(20), location: '48 Wall Street, New York, NY')
+
+
+faves = ["chocolate", "shrimp", "onion soup", "chicken pot pie", "olives", "kimchi", "strawberries", "sushi", "collard greens", "egg salad", "mustard", "sausage", "refried beans", "caesar salad", "grits", "macaroni and cheese", "latkes"]
+
+Party.all.each do |party|
+    5.times do
+    user = party.users.create(email: Faker::Internet.email, password: 'password', password_confirmation: 'password', name: Faker::Name.name, photo_url: Faker::Avatar.image, fave: faves.sample)
   end
 end
 
