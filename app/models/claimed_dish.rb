ae9bb2a6d418d4b4b497eb4d5d44ef8e38dc153d
Debbie Milburn <deborah.a.milburn@gmail.com>
2015-05-12 14:18:44 -0400

    Create find_for_party_and_user method for ClaimedDish

--- app/models/claimed_dish.rb
+++ app/models/claimed_dish.rb
@@ -3,4 +3,8 @@ class ClaimedDish < ActiveRecord::Base
 
   validates :rsvp_id, presence: true
   validates :yummly_id, presence: true
+
+  def self.find_for_party_and_user(party, user)
+    joins(:rsvp).where('rsvps.user_id = ?', user.id).where('rsvps.party_id = ?', party.id)
+  end
 end
