697c490061f22e750d75f212103c30a3243a8854
Harry Park <imharrypark@gmail.com>
2015-05-12 11:29:53 -0400

    specify dropdown events

--- app/assets/javascripts/show-party.js
+++ app/assets/javascripts/show-party.js
@@ -1,14 +1,23 @@
 $(document).ready(function(){
-  $('select').on('change', function(event){
+  $('.new_rsvp select').on('change', function(event){
     event.preventDefault();
+
     $form = $(event.target).closest('.new_rsvp');
-    // debugger
+
     $.ajax({
       url: $form.attr('action'),
       method: $form.attr('method'),
       data: $form.serialize()
     }).then(function(response){
-      $("select").val(response);
+      $otherForms = $(".new_rsvp");
+
+      for (var i=0; i<$otherForms.length; i ++){
+        if ($form.attr('action') === $($otherForms[i]).attr('action')){
+          $($otherForms[i]).find('select').val(response);
+        }
+      }
     });
+
   });
+
 });
