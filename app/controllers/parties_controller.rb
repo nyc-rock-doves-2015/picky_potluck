ec1c1b626d39c70032b286db745c1e6f12987f76
Harry Park <imharrypark@gmail.com>
2015-05-13 09:28:54 -0400

    refactor party attendees

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -4,7 +4,8 @@ class PartiesController < ApplicationController
     if current_user.on_guest_list?(@party)
       @rsvp = Rsvp.new
       @location_query = @party.location.split(' ').join('+')
-      @attendees = @party.users
+      @attendees = @party.users.order(:name)
+      @unregistered_attendees = UnregisteredEmail.find_by(party_id: params[:id])
       @restrictions = @party.combine_nonos
     else
       flash[:notice] = "Sorry, you don't have permission to see that."
