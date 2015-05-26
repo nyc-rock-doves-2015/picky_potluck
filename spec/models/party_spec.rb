a431be142b2844589ab5be747af0231cca547ce2
Debbie Milburn <deborah.a.milburn@gmail.com>
2015-05-26 17:54:53 -0400

    Add party model test for combine_nonos with user with nono

--- spec/models/party_spec.rb
+++ spec/models/party_spec.rb
@@ -4,6 +4,7 @@ describe Party do
 
   let(:party) { FactoryGirl.build :party }
   let(:user) { FactoryGirl.build :user }
+  let(:nono) { FactoryGirl.build :nono }
 
   context "validations" do
     it "should have a name and location" do
@@ -29,6 +30,13 @@ describe Party do
     it "should have no combined_nonos with users without nonos" do
       party.users << user
       expect(party.combine_nonos.empty?).to eq true
+    end
+
+
+    it "should combined_nonos with nonos" do
+      user.nonos << nono
+      party.users << user
+      expect(!party.combine_nonos.empty?).to eq true
     end
   end
 end
